4f9978db77a9692baf5bbc8757aaf56bc1624ac1
MZakiryanov <medet.zakiryanov@gmail.com>
2025-02-24 00:46:40 +0500

    Updates

--- assets/shopping-cart-584d8bf3.js
+++ assets/shopping-cart-584d8bf3.js
@@ -1,7 +1,7 @@
-import{c}from"./index-c74ad2a3.js";/**
+import{c}from"./index-d6bc0935.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
  */const o=[["circle",{cx:"8",cy:"21",r:"1",key:"jimo8o"}],["circle",{cx:"19",cy:"21",r:"1",key:"13723u"}],["path",{d:"M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12",key:"9zh506"}]],r=c("ShoppingCart",o);export{r as S};
-//# sourceMappingURL=shopping-cart-00953bdf.js.map
+//# sourceMappingURL=shopping-cart-584d8bf3.js.map
